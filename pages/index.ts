94f934610a05bda866a3bd319afdee9038dbaa7d
ryankeatonking-cloud <ryan.keaton.king@gmail.com>
2026-03-18 17:49:25 -0700

    Update and rename index.tsx to index.ts

--- pages/index.ts
+++ pages/index.ts
@@ -14,3 +14,8 @@ git push
 export default function Home() {
   return <h1>Keaton King & Co is LIVE</h1>;
 }
+import KeatonKingSite from "../components/KeatonKingSite";
+
+export default function Home() {
+  return <KeatonKingSite />;
+}
