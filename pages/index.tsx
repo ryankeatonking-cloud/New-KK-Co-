fb123f97ab9f1002e5e594bdbfca52782f950e8d
ryankeatonking-cloud <ryan.keaton.king@gmail.com>
2026-03-18 17:33:50 -0700

    Update pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
-import KeatonKingSite from "../components/KeatonKingSite";
+import React from "react";
 
 export default function Home() {
-  return <KeatonKingSite />;
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Keaton King & Co</h1>
+      <p>Your site is now live.</p>
+    </div>
+  );
 }
+git add .
+git commit -m "fix homepage"
+git push
